d576134fe18f3858613f4da548abafc22f6d3f63
Evan Chisholm <elchisholm248@gmail.com>
2023-08-08 21:57:31 -0400

    added support for picking your input files from command line argument

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { tokenize } from "./lib/tokenise";
 import { parse } from "./lib/parser";
 import { evaluate } from "./lib/eval";
 
-const file = Bun.file("./fib.eclisp");
+const file = Bun.file(Bun.argv[2]);
 
 const text = await file.text();
 // console.log(text);
